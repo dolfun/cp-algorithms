6762f04581d541b739d1acac96971f84e540d93a
Dolfun <abhinav150104@gmail.com>
2023-10-03 11:24:31 +0530

    Add template.cc

---
+++ template.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fs first
+#define sn second
+#define pb push_back
+
+using namespace std;
+using str = string;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+template <typename T> using vec = vector<T>;
+template <typename T, typename U> using pr = pair<T, U>;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    return 0;
+}
