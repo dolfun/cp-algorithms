9daf81aa317b525ddad211a7d52a3cc0ab2b895d
Dolfun <abhinav150104@gmail.com>
2023-10-16 02:06:21 +0530

    Add make_primes

--- number-theory/sieve.hpp
+++ number-theory/sieve.hpp
@@ -1,14 +1,25 @@
 #pragma once
 #include <bits/stdc++.h>
 using namespace std;
+template <typename T>
+using vec = vector<T>;
+using ll = long long;
 
-vector<int> make_sieve(size_t N) {
-    vector<int> sieve(N + 1);
-    for (size_t i = 2; (long long)i * i <= N; ++i) {
+auto make_sieve(size_t N) {
+    vec<int> sieve(N + 1);
+    for (size_t i = 2; (ll)i * i <= N; ++i) {
         if (sieve[i]) continue;
         for (size_t x = 2 * i; x <= N; x += i) {
             sieve[x] = static_cast<int>(i);
         }
     }
     return sieve;
+}
+
+auto make_primes(size_t N) {
+    auto sieve = make_sieve(N);
+    vec<int> primes;
+    for (int i = 2; i <= N; ++i)
+        if (!sieve[i]) primes.push_back(i);
+    return primes;
 }
