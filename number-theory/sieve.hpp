90d379251a61dc832bdc7a698f6daffc472f9bc6
Dolfun <abhinav150104@gmail.com>
2023-10-16 04:40:59 +0530

    Re-add accidental deletion

--- number-theory/sieve.hpp
+++ number-theory/sieve.hpp
@@ -13,6 +13,14 @@ auto make_sieve(size_t N) {
     }
     return sieve;
 }
+
+auto make_primes(size_t N) {
+    auto sieve = make_sieve(N);
+    vec<int> primes;
+    for (int i = 2; i <= N; ++i)
+        if (!sieve[i]) primes.push_back(i);
+    return primes;
+}
  
 auto make_primes(size_t N) {
     vec<int> sieve(N + 1);
