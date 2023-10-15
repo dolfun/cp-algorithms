04819d987132d844bc9de6a9e91c5cbd5c09e0aa
Dolfun <abhinav150104@gmail.com>
2023-10-16 04:40:14 +0530

    Add an independent version of make_primes

--- number-theory/sieve.hpp
+++ number-theory/sieve.hpp
@@ -13,9 +13,13 @@ auto make_sieve(size_t N) {
     }
     return sieve;
 }
-
+ 
 auto make_primes(size_t N) {
-    auto sieve = make_sieve(N);
+    vec<int> sieve(N + 1);
+    for (size_t i = 2; (ll)i * i <= N; ++i) {
+        if (sieve[i]) continue;
+        for (size_t x = 2 * i; x <= N; x += i) sieve[x] = i;
+    }
     vec<int> primes;
     for (int i = 2; i <= N; ++i)
         if (!sieve[i]) primes.push_back(i);
