8ae1c0ca5432395110a1b2b0c45011e05eaf2a8e
Dolfun <abhinav150104@gmail.com>
2023-11-01 05:11:04 +0530

    Update template

--- template.cc
+++ template.cc
@@ -2,14 +2,23 @@
 #define fs first
 #define sn second
 #define pb push_back
+#define mp make_pair
 
 using namespace std;
+#if (__cplusplus >= 202002L)
+namespace R = ranges;
+namespace V = views;
+#endif
+
 using str = string;
-using uint = unsigned int;
 using ll = long long;
+using ld = long double;
+using uint = unsigned int;
 using ull = unsigned long long;
 template <typename T> using vec = vector<T>;
-template <typename T, typename U> using pr = pair<T, U>;
+template <typename T, typename U>using pr = pair<T, U>;
+template <typename T, typename U>
+using umap = unordered_map<T, U>;
 
 int main() {
 #ifndef ONLINE_JUDGE
