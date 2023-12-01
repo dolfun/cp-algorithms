bc28b71d5832c365855c894b217eb28d52f4ab2b
Dolfun <abhinav150104@gmail.com>
2023-12-02 00:39:42 +0530

    Change indent size from 4 spaces to 2 spaces

--- sieve.hpp
+++ sieve.hpp
@@ -6,30 +6,19 @@ using vec = vector<T>;
 using ll = long long;
 
 auto make_sieve(size_t N) {
-    vec<int> sieve(N + 1);
-    for (size_t i = 2; (ll)i * i <= N; ++i) {
-        if (sieve[i]) continue;
-        for (size_t x = 2 * i; x <= N; x += i) sieve[x] = i;
-    }
-    return sieve;
+  vec<int> sieve(N + 1);
+  for (size_t i = 2; i * i <= N; ++i) {
+    if (sieve[i]) continue;
+    for (size_t x = 2 * i; x <= N; x += i)
+      sieve[x] = i;
+  }
+  return sieve;
 }
 
 auto make_primes(size_t N) {
-    auto sieve = make_sieve(N);
-    vec<int> primes;
-    for (int i = 2; i <= N; ++i)
-        if (!sieve[i]) primes.push_back(i);
-    return primes;
-}
-
-auto make_primes(size_t N) {
-    vec<int> sieve(N + 1);
-    for (size_t i = 2; (ll)i * i <= N; ++i) {
-        if (sieve[i]) continue;
-        for (size_t x = 2 * i; x <= N; x += i) sieve[x] = i;
-    }
-    vec<int> primes;
-    for (int i = 2; i <= N; ++i)
-        if (!sieve[i]) primes.push_back(i);
-    return primes;
+  auto sieve = make_sieve(N);
+  vec<int> primes;
+  for (size_t i = 2; i <= N; ++i)
+    if (!sieve[i]) primes.push_back(i);
+  return primes;
 }
