d62003b2c921c37553291fa3e69705c67fd8c2b1
Dolfun <abhinav150104@gmail.com>
2023-12-02 00:43:55 +0530

    Add some utility functions

---
+++ utility.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+using vec = vector<T>;
+
+template <typename T>
+auto vec_of_vec(size_t n, size_t m) {
+  return vec<vec<T>>(n, vec<T>(m));
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const vec<T>& v) {
+  for (const auto& x : v)
+    out << x << ' ';
+  return out;
+}
+
+template <typename T>
+istream& operator>>(istream& in, vec<T>& v) {
+  for (auto& x : v)
+    in >> x;
+  return in;
+}
+
+template <typename Arg, typename... Args>
+void print(Arg&& arg, Args&&... args) {
+  cout << std::forward<Arg>(arg);
+  ((cout << ' ' << std::forward<Args>(args)), ...);
+}
