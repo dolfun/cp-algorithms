3f4965013e2eadf6324eec947bc22cefd23ba122
Dolfun <abhinav150104@gmail.com>
2023-12-02 02:52:54 +0530

    Fix indentation

--- short_template.cc
+++ short_template.cc
@@ -4,8 +4,8 @@ using ll = long long;
 
 int main() {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	// freopen("output.txt", "w", stdout);
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
 #endif
     
   return 0;
