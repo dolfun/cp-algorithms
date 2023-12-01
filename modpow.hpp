7b7e20bb9b96072d9ac23ad0f89deb0b50887323
Dolfun <abhinav150104@gmail.com>
2023-12-02 00:30:32 +0530

    Move gcd.hpp, modpow.hpp and sieve.hpp to project root

---
+++ modpow.hpp
@@ -0,0 +1,35 @@
+#pragma once
+using ll = long long;
+
+ll mod = 1'000'000'007;
+ll modpow(ll x, ll n) {
+    if (n == 0) return 1;
+    ll res = modpow(x, n / 2);
+    res = (res * res) % mod;
+    if (n % 2 == 1) res = (res * x) % mod;
+    return res;
+}
+
+ll modinv(ll x) {
+    return modpow(x, mod - 2);
+}
+
+ll modpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    ll res = modpow(x, n / 2, mod);
+    res = (res * res) % mod;
+    if (n % 2 == 1) res = (res * x) % mod;
+    return res;
+}
+
+ll modinv(ll x, ll mod) {
+    return modpow(x, mod - 2);
+}
+
+ll intpow(ll x, ll n) {
+    if (n == 0) return 1;
+    ll res = intpow(x, n / 2);
+    res *= res;
+    if (n % 2 == 1) res *= x;
+    return res;
+}
