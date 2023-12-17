9b045fb3ed5ff3cca32c224940b00582ca988088
Dolfun <abhinav150104@gmail.com>
2023-12-18 04:16:00 +0530

    Add modmult

--- modpow.hpp
+++ modpow.hpp
@@ -1,6 +1,8 @@
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
 
-const ll mod = 1'000'000'007;
+constexpr ll mod = 1'000'000'007;
 ll modpow(ll x, ll n, ll mod = mod) {
   if (n == 0) return 1;
   ll res = modpow(x, n / 2, mod);
@@ -19,4 +21,14 @@ ll powll(ll x, ll n) {
   res *= res;
   if (n % 2 == 1) res *= x;
   return res;
+}
+
+ull modmult(ull a, ull b, ull mod = mod) {
+  ld x; ull c; ll r;
+  if (a >= mod) a %= mod;
+  if (b >= mod) b %= mod;
+  x = a;
+  c = x * b / mod;
+  r = (ll)(a * b - c * mod) % (ll)mod;
+  return r < 0 ? r + mod : r;
 }
