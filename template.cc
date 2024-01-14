38e32806b97a4248997f4aa3bee08c544c4576dc
Dolfun <abhinav150104@gmail.com>
2024-01-14 22:22:34 +0530

    Remove newline

--- template.cc
+++ template.cc
@@ -18,9 +18,8 @@ using ull = unsigned long long;
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template <typename T>
-using ordered_set = 
-  tree<T, null_type, less<T>,
-    rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_set = tree<T, null_type, less<T>,
+  rb_tree_tag, tree_order_statistics_node_update>;
 #endif
 
 #if (__cplusplus >= 202002L)
