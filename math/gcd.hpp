57d4c68017da4dbdb3a121f46b8e683462fa9431
Dolfun <abhinav150104@gmail.com>
2024-01-25 01:42:10 +0530

    Change unsigned int to uint32_t

--- math/gcd.hpp
+++ math/gcd.hpp
@@ -21,9 +21,9 @@ T gcd_extended(T a, T b, T& x, T& y) {
 }
 
 [[gnu::target("popcnt")]]
-unsigned int fast_gcd(unsigned int a, unsigned int b) {
+uint32_t fast_gcd(uint32_t a, uint32_t b) {
   if (!a || !b) return a | b;
-  unsigned int shift = __builtin_ctz(a | b);
+  uint32_t shift = __builtin_ctz(a | b);
   a >>= __builtin_ctz(a);
   do {
     b >>= __builtin_ctz(b);
